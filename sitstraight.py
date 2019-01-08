e71e959becc8e0a3c451697a8b24f74e5784dcda
Vince Lwt <vince@lyser.io>
2019-01-08 21:12:48 +0100

    First working version

--- sitstraight.py
+++ sitstraight.py
@@ -1,22 +1,33 @@
 import rumps
 from subprocess import Popen, PIPE
 
-class SitStraight(rumps.App):
+#rumps.debug_mode(True)
 
-	@rumps.clicked("Enable reminders")
-	def onoff(self, sender):
-		sender.state = not sender.state
+defaultTimeout = 60*10
 
-	@rumps.clicked("Test notification")
-	def sayhi(self, _):
-		cmd = b"""display notification "Come on' man" with title "Dude, sit straight" """
-		Popen(["osascript", '-'], stdin=PIPE, stdout=PIPE).communicate(cmd)
-		
-		rumps.notification("Hey! Are you sitting straight?!", "I thought so", None, True, "Yes", "Nop..")
+def notif(sender):
+	print 'Sending notification'
+	cmd = b"""display notification "Come on' man" with title "Dude, sit straight" subtitle "" """
+	Popen(["osascript", '-'], stdin=PIPE, stdout=PIPE).communicate(cmd)
 
-	@rumps.clicked("About")
-	def prefs(self, _):
-		rumps.alert("SitStraight is an experiment by Lyser.io")
+@rumps.clicked("Enable reminders")
+def onoff(sender):
+	sender.state = not sender.state
+	if not sender.state:
+	 	global_namespace_timer.stop()
+	else:
+	 	global_namespace_timer.start()
+
+@rumps.clicked("About")
+def about(_):
+	rumps.alert("SitStraight is an experiment by Lyser.io\nGive a star on https://github.com/vincelwt/sitstraight")
 
 if __name__ == "__main__":
-	SitStraight("SitStraight").run()
+	global_namespace_timer = rumps.Timer(notif, defaultTimeout)
+
+	# Manually create the button to force default state
+	button = rumps.MenuItem("Enable reminders")
+	onoff(button)
+
+	rumps.App('SitStraight', menu=(button, 'About')).run()
+
